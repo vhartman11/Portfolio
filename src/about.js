88af26cd80e6e8a1956721b2ed5adb7494199e3a
John Hartman <35504444+JohnHartman939@users.noreply.github.com>
2022-11-16 09:19:50 -0500

    just an example of a reuasable component

--- src/about.js
+++ src/about.js
@@ -6,6 +6,23 @@ import Fishing    from './images/fish.jpg';
 import Homestead  from './images/homestead.jpg';
 import Inspection from './images/inspection.jpg';
 
+
+function AboutCard(props) {
+  return (
+    <div class="col-md-6 col-lg-3">
+    <div class="card bg-light">
+      <div class="card-body text-center">
+        <img src={props.imgSrc} alt={props.imgAlt} class="img-fluid rounded-circle mb-3" />
+        <h3 class="card-title mb-3">{props.cardTitle}</h3>
+        <p class="card-text">{props.cardText}</p>
+        {/* this is still in development, but i would like to add a map of some places that I have fished and a small photo album */}
+        {/* <a href="fished map.html"><i class="bi bi-map text-dark mx-1" style="font-size: 2rem"></i></a> */}
+        {/* <a href="fish pics.html"><i class="bi bi-images text-dark mx-1" style="font-size: 2rem"></i></a> */}
+      </div>
+    </div>
+  </div>
+  )
+}
 export function About() {
   return(
     <>
@@ -33,18 +50,7 @@ export function About() {
         <div class="row g-4">
 
           {/* this is the far left box */}  
-          <div class="col-md-6 col-lg-3">
-            <div class="card bg-light">
-              <div class="card-body text-center">
-                <img src={Fishing} alt="9.5 pound carp" class="img-fluid rounded-circle mb-3" />
-                <h3 class="card-title mb-3">Fishing</h3>
-                <p class="card-text">I try to go out whenever I can.  I enjoy catching all species of fish.</p>
-                {/* this is still in development, but i would like to add a map of some places that I have fished and a small photo album */}
-                {/* <a href="fished map.html"><i class="bi bi-map text-dark mx-1" style="font-size: 2rem"></i></a> */}
-                {/* <a href="fish pics.html"><i class="bi bi-images text-dark mx-1" style="font-size: 2rem"></i></a> */}
-              </div>
-            </div>
-          </div>
+          <AboutCard cardTitle='React' cardText='I love learning about React' imgSrc={Fishing} imgAlt='9.5 pound carp'></AboutCard>
 
           {/* this is the middle left box */}
           <div class="col-md-6 col-lg-3">
